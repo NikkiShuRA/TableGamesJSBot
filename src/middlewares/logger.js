480da2f3e4f89266af524f076f043e2ab83357fe
NikkiShuRA <xak-bag-303@yandex.ru>
2026-03-04 18:26:36 +0500

    Изменение логирования

--- src/middlewares/logger.js
+++ src/middlewares/logger.js
@@ -1,6 +1,11 @@
+const pino = require('pino');
 
 function createTelegrafLogger(options = {}) {
-    
+    const logger = pino({ level: process.env.LOG_LEVEL || 'info', ...options });
+    return (ctx, next) => {
+        logger.info({ ctx }, 'incoming update');
+        return next();
+    };
 }
 
 module.exports = createTelegrafLogger;
